bd9489712610319c0bea70ea687e260f7618146b
juusomih <juuso.miikka.heikkila@gmail.com>
2026-08-01 18:14:24 +0300

    Fixed orientation back to the boring normal, up is up and down is down.

--- Noita.py
+++ Noita.py
@@ -1,31 +1,39 @@
 import numpy, pygame, random
-HEIGHT, WIDTH = 640, 480
+WIDTH, HEIGHT = 640, 480
+ILMA = 0
 HIEKKA = 100
 VESI = 255
 
-testi = numpy.zeros((HEIGHT, WIDTH))
+testi = numpy.full((HEIGHT, WIDTH),ILMA)
 
 pygame.init()
 
-naytto = pygame.display.set_mode((HEIGHT, WIDTH))
+naytto = pygame.display.set_mode((WIDTH, HEIGHT))
 kello = pygame.time.Clock()
 bgcolor = 0
 
-def hiekkafysiikka(array):
+def hiekka_fysiikka(array):
     for row in range(array.shape[0] - 2, -1, -1):
-            can_move = (array[row] == HIEKKA) & (array[row + 1] == 0)
-            array[row][can_move] = 0
-            array[row + 1][can_move] = HIEKKA
+        can_move = (array[row] == HIEKKA) & (array[row + 1] == ILMA)
+        array[row][can_move] = ILMA
+        array[row + 1][can_move] = HIEKKA
 
     return array
 
+def vesi_fysiikka(array):
+    for row in range(array.shape[0] - 2, -1, -1):
+        can_move = (array[row] == VESI) & (array[row + 1] == ILMA)
+        array[row][can_move] = ILMA
+        array[row + 1][can_move] = VESI
+    
+    return array
 
 while True:
     for tapahtuma in pygame.event.get():
         
         if tapahtuma.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
-            testi[x,y] = HIEKKA
+            testi[y,x] = VESI
         if tapahtuma.type == pygame.QUIT:
             exit()
         
@@ -33,10 +41,11 @@ while True:
             if tapahtuma.key == pygame.K_ESCAPE:
                 exit()       
         
-    testi = hiekkafysiikka(testi)
+    testi = hiekka_fysiikka(testi)
+    testi = vesi_fysiikka(testi)
 
     
-    surface = pygame.surfarray.make_surface(testi)
+    surface = pygame.surfarray.make_surface(testi.T)
     naytto.blit(surface, (0, 0))
     pygame.display.flip()
 
